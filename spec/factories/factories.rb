fc18335f14bf8e068026c6370b3e9eaee818bd0b
Eduardo Maia <eduvimaia@gmail.com>
2018-02-09 21:42:16 -0200

    added factories

---
+++ spec/factories/factories.rb
@@ -0,0 +1,38 @@
+FactoryBot.define do
+  factory :user, class: Telegram::Bot::Types::User do
+    id            { rand(1000) }
+
+    first_name    "Eduardo"
+    last_name     "Maia"
+    username      "emaiax"
+    language_code "en-BR"
+  end
+
+  factory :chat, class: Telegram::Bot::Types::Chat do
+    id         { rand(1000) }
+
+    first_name "Eduardo"
+    last_name  "Maia"
+    username   "emaiax"
+    type       "private"
+
+    trait :group do
+      type "group"
+    end
+  end
+
+  factory :message, class: Telegram::Bot::Types::Message do
+    message_id { rand(1000) }
+
+    association :from, factory: :user, strategy: :build
+    association :chat, factory: :chat, strategy: :build
+  end
+
+  factory :callback, class: Telegram::Bot::Types::CallbackQuery do
+    id { rand(1000) }
+
+    association :from,          factory: :user,    strategy: :build
+    association :message,       factory: :message, strategy: :build
+    association :chat_instance, factory: :chat,    strategy: :build
+  end
+end
